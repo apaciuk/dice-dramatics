d94e1c93646e897433066416f191be94da05680a
Paul Anthony McGowan <xhostcom@gmail.com>
2022-05-29 09:31:29 +0100

    Refine tests

--- lib/dice_dramatics/totals.rb
+++ lib/dice_dramatics/totals.rb
@@ -3,7 +3,11 @@
 module DiceDramatics
   class Totals
     def total_dice(number_of_sides, _number_of_dice = 1, _opts = {})
-      result = rand(number_of_sides) + 1
+      total = 0
+      number_of_dice.times do
+        total += rand(number_of_sides) + 1
+      end
+      total
     end
   end
 end
