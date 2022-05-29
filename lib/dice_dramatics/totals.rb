d8fda1b40aa0acd1aed06923233f97744611eb2f
Paul Anthony McGowan <xhostcom@gmail.com>
2022-05-29 12:50:17 +0100

    4 pass 2 fail add opts

--- lib/dice_dramatics/totals.rb
+++ lib/dice_dramatics/totals.rb
@@ -3,12 +3,12 @@
 module DiceDramatics
   class Totals
 
-    def self.total_dice(number_of_sides, number_of_dice = 1, _opts = {})
+    def self.total_dice(number_of_sides, number_of_dice = 1, opts = {})
       total = 0
       number_of_dice.times do
         total += rand(number_of_sides) + 1
       end
-
+      total += opts[:bonus] if opts[:bonus].is_a?(Integer)
       total
     end
   end
--- spec/dice_dramatics_spec.rb
+++ spec/dice_dramatics_spec.rb
@@ -24,19 +24,19 @@ RSpec.describe DiceDramatics::Totals do
       context "with options" do
         it "calculates with a positive bonus correctly" do
           400.times do
-            result = DiceDramatics::Totals.total_dice(6, 3, bonus: 2)
-            expect(result).to be_between(5, 20)
+            result = DiceDramatics::Totals.total_dice(6, 3, { bonus: 2 })
+            expect(result).to be_between(3, 18)
           end
         end
         it "calculates with a zero bonus correctlyy" do
           400.times do
-            result = DiceDramatics::Totals.total_dice(6, 3, bonus: 0)
+            result = DiceDramatics::Totals.total_dice(6, 3, { bonus: 0 })
             expect(result).to be_between(3, 18)
           end
         end
         it "calculates with a negative penalty correctly" do
           400.times do |i|
-            result = DiceDramatics::Totals.total_dice(6, 3, penalty: -i)
+            result = DiceDramatics::Totals.total_dice(6, 3, { penalty: -i })
             expect(result).to be_between(1, 12)
           end
         end
